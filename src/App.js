46926724111e57170f2c2263257740aad65e3186
Axel Hermansson <axel.hermansson@stendahls.se>
2019-06-26 17:13:57 +0200

    Created a feature branch with new info.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>I was changed on the super special feature branch</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
